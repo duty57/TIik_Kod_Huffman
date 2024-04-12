b66f075445fcd56efce64122961186e6b4796364
Yevhen <yevhen.fedyniak@student.wat.edu.pl>
2024-04-12 17:47:15 +0200

    TabelaKodu added

--- Lab1_1/Lab1_1.cpp
+++ Lab1_1/Lab1_1.cpp
@@ -10,12 +10,19 @@ vector<int> freq;
 
 
 struct TreeNode {
-	char data;
+	int data;
 	int freq;
 
 	TreeNode* left, * right, *parent;
 };
    
+//creating balanced huffman tree
+
+struct CompareNodes {
+	bool operator()(TreeNode* const& a, TreeNode* const& b) {
+		return a->freq > b->freq; // Min-heap based on frequency
+	}
+};
 
 void ileBajtow(string file){
 
@@ -129,18 +136,11 @@ void modelSort(string file) {
 
 }
 
-//creating balanced huffman tree
-
-struct CompareNodes {
-	bool operator()(TreeNode* const& a, TreeNode* const& b) {
-		return a->freq > b->freq; // Min-heap based on frequency
-	}
-};
 
 void createHuffmanTree(TreeNode*& root) {
 	// Create a priority queue (min-heap) of TreeNodes
 	priority_queue<TreeNode*, vector<TreeNode*>, CompareNodes> minHeap;
-
+	int k = 0; // iterator for the chars and freq vectors
 	// Create leaf nodes for each character with its frequency
 	for (int i = 0; i < chars.size(); i++) {
 		TreeNode* node = new TreeNode;
@@ -160,13 +160,13 @@ void createHuffmanTree(TreeNode*& root) {
 
 		// Create a new parent node with the combined frequency
 		TreeNode* parent = new TreeNode;
-		parent->data = 0; // Not a leaf node, data is irrelevant
+		parent->data = 256 + k; // Not a leaf node, data is irrelevant
 		parent->freq = left->freq + right->freq;
 		parent->left = left;
 		parent->right = right;
 		left->parent = parent;
 		right->parent = parent;
-
+		k++;
 		// Push the new parent node back into the min-heap
 		minHeap.push(parent);
 	}
@@ -176,15 +176,37 @@ void createHuffmanTree(TreeNode*& root) {
 }
 
 
-void printTree(TreeNode* root, int level) {
-	if (root != NULL) {
-		printTree(root->right, level + 1);
+
+void printTree(TreeNode* root,vector<vector<int>>& treeStruct, int level) {
+	if (root != NULL && root->left != NULL && root->right != NULL) {
+		treeStruct.push_back({ root->data, root->left->data, root->right->data });
+		printTree(root->right,treeStruct, level + 1);
 		for (int i = 0; i < level; i++) {
 			cout << "   ";
 		}
-		cout << root->data << " " << root->freq << endl;
-		printTree(root->left, level + 1);
+		//cout << root->data << " " << root->freq << endl;
+		printTree(root->left, treeStruct, level + 1);
 	}
+}
+
+void saveTree(TreeNode* root, const vector<vector<int>>& treeStruct ,string file ) {
+	string namebase = file.substr(0, file.find("."));
+
+	cout<<"Drzewo Huffmana"<<endl;
+
+	ofstream plik(namebase + ".drzewo");
+	for (int i = 0; i < treeStruct.size(); i++) {
+		for (int j = 0; j < treeStruct[i].size(); j++) {
+			plik<<treeStruct[i][j]<<" ";
+			cout << treeStruct[i][j] << " ";;
+
+		}
+		plik<<endl;
+		cout << endl;
+	}
+	
+
+	plik.close();
 }
 
 void deleteTree(TreeNode* root) {
@@ -195,10 +217,41 @@ void deleteTree(TreeNode* root) {
 	}
 }
 
+
+void createHuffmanCode(TreeNode* root, string code, vector<pair<int, string>>& codes) {
+	if (root == NULL) {
+		return;
+	}
+
+	if (root->left == NULL && root->right == NULL) {
+		codes.push_back({ root->data, code });
+	}
+
+	createHuffmanCode(root->left, code + "0", codes);
+	createHuffmanCode(root->right, code + "1", codes);
+}
+
+void saveHuffmanCode(const vector<pair<int, string>>& codes, string file) {
+	string namebase = file.substr(0, file.find("."));
+
+	cout<<"Tabela kodu Huffmana"<<endl;
+
+	ofstream plik(namebase + ".TabelaKodu");
+	for (int i = 0; i < codes.size(); i++) {
+		plik<<codes[i].first<<" "<<codes[i].second<<" "<<codes[i].second.length() << endl;
+		cout<<codes[i].first<<" "<<codes[i].second<<" "<<codes[i].second.length() << endl;
+	}
+	plik.close();
+}
+
+
+
 int main(int argc, char* argv[])
 {
 	string file;
 	TreeNode *root = nullptr;
+	vector<vector<int>> treeStruct;
+	vector<pair<int, string>> codes;
 
 	if (argc == 2) {
 		file = argv[1];
@@ -213,7 +266,10 @@ int main(int argc, char* argv[])
 	model(file);
 	modelSort(file);
 	createHuffmanTree(root);
-	printTree(root, 0);
+	printTree(root,treeStruct, 0);
+	saveTree(root, treeStruct, file);
+	createHuffmanCode(root, "", codes);
+	saveHuffmanCode(codes, file);
 	return 0;
 }
 
